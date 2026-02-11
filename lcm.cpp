00dccf4c3dca807f87ad49742a25fed9c4c655ff
shreyaeve <shreyaeve1912@gmail.com>
2026-02-11 17:05:51 +0000

    lcm

---
+++ lcm.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a;
+    cin>>b;
+    int maxi= max(a,b);
+    int mini=min(a,b);
+    int i=1;
+    while(true)
+    { 
+        cout<<"hi from loop"<<endl;
+        if(maxi*i%mini==0){
+            cout<<"lcm is"<<maxi*i;
+            break;
+        }
+        i++;
+    }
