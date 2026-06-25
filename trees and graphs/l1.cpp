42ea2f2daf01df906795286bea0b08442b31e8e0
shreyaeve <shreyaeve1912@gmail.com>
2026-06-25 12:34:00 +0000

    window questions

--- trees and graphs/l1.cpp
+++ trees and graphs/l1.cpp
@@ -18,7 +18,7 @@ using namespace std;
 // int arr[]={1,1,0,0,0,1,1,0,1,1,1};
 // int n=sizeof(arr)/sizeof(arr[0]);
 // int count=0;
-// for(int i =0;i<n-k;i++){
+// for(int i =0;i<=n-k;i++){
 //     if(arr[i]==0){
 //         cout<<"if accepted, enter the loop "<<k<< "times"<<endl;
 //         for(int j=i;j<i+k;j++){
@@ -70,3 +70,60 @@ using namespace std;
 //         count++;
 //     }
 
+// int main(){
+//     queue <int> q;
+//     queue <int> q1;
+//     queue <int> q2;
+
+//     q.push(5);
+//     q.push(10);
+//     q.push(2);
+//     q.push(4);
+//     // q.push(7);
+//     // q.push(5);
+
+// int n= q.size();
+
+// for(int i =0;i<n/2;i++){
+//     q1.push(q.front());
+//     q.pop();
+// }
+// //spliited half elements to new queue
+
+// for(int i =0;i<n/2;i++){
+//     q2.push(q1.front());
+//     q2.push(q.front());
+//     q1.pop();
+//     q.pop();
+// }
+
+// while(q2.size()!=0){
+//     cout<<q2.front();
+//     q2.pop();
+// }
+
+// }
+
+int main(){
+    int arr[]= {-8,2,3,-6,1};
+    int k =2;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int result[n-k+1]={0};
+    cout<<"size"<<n<<endl;
+
+
+    for(int i=0;i<=n-k;i++){
+        cout<<"outer loop run "<< i<<endl;
+        for(int j=i;j<i+k;j++){
+            cout<<" inner loop "<<i<<endl;
+            if(arr[j]<0){
+                result[i]=arr[j];
+                break;
+            }
+        }
+    }
+
+    for(int i=0;i<n-k+1;i++){
+        cout<<result[i]<<" ";
+    }
+}
